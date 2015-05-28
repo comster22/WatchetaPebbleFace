7b208275b396325942346df16ad99e10f8b683b6
Vishal <rsh.vishal@gmail.com>
2015-05-27 17:03:45 -0700

    Added weather.js and included weather in the watch

--- src/main.c
+++ src/main.c
@@ -1,8 +1,15 @@
 #include <pebble.h>
-
+#define KEY_TEMPERATURE 0
+#define KEY_CONDITIONS 1
+  
 static Window *s_main_window;
+
 static TextLayer *s_time_layer;
+static TextLayer *s_weather_layer;
+
 static GFont s_time_font;
+static GFont s_weather_font;
+
 static BitmapLayer *s_background_layer;
 static GBitmap *s_background_bitmap;
 
@@ -29,30 +36,55 @@ static void update_time() {
 
 static void tick_handler(struct tm *tick_time, TimeUnits units_changed) {
   update_time();
+  // Get weather update every 60 minutes
+  if(tick_time->tm_min % 60 == 0) {
+    // Begin dictionary
+    DictionaryIterator *iter;
+    app_message_outbox_begin(&iter);
+  
+    // Add a key-value pair
+    dict_write_uint8(iter, 0, 0);
+  
+    // Send the message!
+    app_message_outbox_send();
+  }  
 }
 
 static void main_window_load(Window *window) {
   
   // Create GBitmap, then set to created BitmapLayer
   s_background_bitmap = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_DBZ);
-  s_background_layer = bitmap_layer_create(GRect(-25, 15, 144, 168));
+  s_background_layer = bitmap_layer_create(GRect(-25, 0, 144, 168));
   bitmap_layer_set_bitmap(s_background_layer, s_background_bitmap);
   layer_add_child(window_get_root_layer(window), bitmap_layer_get_layer(s_background_layer));
   
-  //   Create time TextLayer
-  s_time_layer = text_layer_create(GRect(30, 35, 144, 50));
+  // Create time TextLayer
+  s_time_layer = text_layer_create(GRect(35, 35, 144, 50));
   text_layer_set_background_color(s_time_layer, GColorClear);
   text_layer_set_text_color(s_time_layer, GColorBlack);
   
-  //   Improve the layout to be more like a watchface
+  // Improve the layout to be more like a watchface
   // Create GFont
-  s_time_font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_CHEWY_28));
+  s_time_font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_CHEWY_30));
   text_layer_set_font(s_time_layer, s_time_font);
   text_layer_set_text_alignment(s_time_layer, GTextAlignmentCenter);
-  
-  //   Add it to the child layer to the Window's root layer
+   
+  // Add it to the child layer to the Window's root layer
   layer_add_child(window_get_root_layer(window), text_layer_get_layer(s_time_layer));
+  
+  // Create weather TextLayer
+  s_weather_layer = text_layer_create(GRect(35, 70, 144, 50));
+  text_layer_set_background_color(s_weather_layer, GColorClear);
+  text_layer_set_text_color(s_weather_layer, GColorBlack);
+  text_layer_set_text_alignment(s_weather_layer, GTextAlignmentCenter);
+  text_layer_set_text(s_weather_layer, "Loading...");
+  
+  // Create second custom font, apply it and add to the Window
+  s_weather_font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_CHEWY_18));
+  text_layer_set_font(s_weather_layer, s_weather_font);
+  layer_add_child(window_get_root_layer(window), text_layer_get_layer(s_weather_layer));
 }
+  
 
 static void main_window_unload(Window *window) {
   // Destroy TextLayer
@@ -63,8 +95,55 @@ static void main_window_unload(Window *window) {
   gbitmap_destroy(s_background_bitmap);
   // Destroy BitmapLayer
   bitmap_layer_destroy(s_background_layer);
+  // Destroy weather elements
+  text_layer_destroy(s_weather_layer);
+  fonts_unload_custom_font(s_weather_font);
 }
-  
+
+static void inbox_received_callback(DictionaryIterator *iterator, void *context) {
+  // Store incoming information
+  static char temperature_buffer[8];
+  static char conditions_buffer[32];
+  static char weather_layer_buffer[32];
+
+  // Read first item
+  Tuple *t = dict_read_first(iterator);
+
+  // For all items
+  while(t != NULL) {
+    // Which key was received?
+    switch(t->key) {
+      case KEY_TEMPERATURE:
+        snprintf(temperature_buffer, sizeof(temperature_buffer), "%dF", (int)t->value->int32);
+        break;
+      case KEY_CONDITIONS:
+        snprintf(conditions_buffer, sizeof(conditions_buffer), "%s", t->value->cstring);
+        break;
+      default:
+        APP_LOG(APP_LOG_LEVEL_ERROR, "Key %d not recognized!", (int)t->key);
+        break;
+    }
+
+    // Look for next item
+    t = dict_read_next(iterator);
+  }
+  // Assemble full string and display
+  snprintf(weather_layer_buffer, sizeof(weather_layer_buffer), "%s, %s", temperature_buffer, conditions_buffer);
+  text_layer_set_text(s_weather_layer, weather_layer_buffer);
+}
+
+static void inbox_dropped_callback(AppMessageResult reason, void *context) {
+  APP_LOG(APP_LOG_LEVEL_ERROR, "Message dropped!");
+}
+
+static void outbox_failed_callback(DictionaryIterator *iterator, AppMessageResult reason, void *context) {
+  APP_LOG(APP_LOG_LEVEL_ERROR, "Outbox send failed!");
+}
+
+static void outbox_sent_callback(DictionaryIterator *iterator, void *context) {
+  APP_LOG(APP_LOG_LEVEL_INFO, "Outbox send success!");
+}
+
 static void init() {
   //   Create new Window element
   s_main_window = window_create();
@@ -88,6 +167,15 @@ static void init() {
   
   // Make sure the time is displayed from the start
   update_time();
+  
+  // Register callbacks
+  app_message_register_inbox_received(inbox_received_callback);
+  // Open AppMessage
+  app_message_open(app_message_inbox_size_maximum(), app_message_outbox_size_maximum());
+
+  app_message_register_inbox_dropped(inbox_dropped_callback);
+  app_message_register_outbox_failed(outbox_failed_callback);
+  app_message_register_outbox_sent(outbox_sent_callback);
 }
 
 static void deinit() {
